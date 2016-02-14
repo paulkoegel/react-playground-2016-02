134aceae463ad5f3e779c67765708530d4b7e283
Paul Kögel <paul@railslove.com>
2016-02-14 11:07:51 +0100

    App.js: add link to source on Github.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router';
 
-export default React.createClass({
+export default class extends React.Component {
   render() {
     return <div className='mainWrapper'>
     <nav>
@@ -10,8 +10,10 @@ export default React.createClass({
       <Link to={'/03'} activeClassName='active'>03-list</Link>
       <Link to={'/04'} activeClassName='active'>04-select</Link>
       <Link to={'/05'} activeClassName='active'>05-add-book</Link>
+      <Link to={'/99'} activeClassName='active'>99-es6-7</Link>
+      <a href='https://github.com/paulkogel/react-playground-2016-02/'>Github</a>
     </nav>
       { this.props.children }
     </div>
   }
-})
+}
